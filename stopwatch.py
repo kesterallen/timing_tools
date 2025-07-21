f7032ebc0451e5492bb7dbaf6ba1cc57e5a42d0d
Kester Allen <kester@gmail.com>
2025-07-21 10:00:15 -0700

    Refactor for clarity

--- stopwatch.py
+++ stopwatch.py
@@ -2,43 +2,53 @@
 
 import curses
 from curses import A_BOLD, A_NORMAL
-from datetime import datetime
+import datetime as dt
 import sys
 
 COL = 0
-HEADER = ["Stopwatch: q to quit, space to mark a lap", "", "Time       lap (#)   total"]
+HEADER = ["Stopwatch: q to quit, space to mark a lap, u to undo a mark", "", "Time       lap (#)   total"]
 HEADER_ROWS = len(HEADER)
 
+BLANK_LINE = "                                          "
+BUFFER_ROWS = 0
 
-def tds(td):
+is_bold = False
+
+
+def tds(td: dt.timedelta):
     """The number of seconds in the time delta in %.1f precision"""
     return f"{td.total_seconds():.1f}"
 
 
-def hms(dt):
+def hms(dt: dt.datetime):
     """Hours:Minutes:Seconds string for a datetime"""
     return dt.strftime("%H:%M:%S")
 
 
-def fmt(is_bold):
+def fmt(is_bold: bool):
     """Curses formatting attribute"""
     return A_BOLD if is_bold else A_NORMAL
 
+def write_row(line: str, lap_num: int, is_bold: bool, screen: curses.window) -> None:
+    """Write 'line' to the current row on the screen"""
+    row = HEADER_ROWS + lap_num % BUFFER_ROWS
+    screen.addstr(row, COL, line, fmt(is_bold))
 
-def main(screen):
-    """Main stopwatch function"""
+def init(screen):
+    global BUFFER_ROWS
+    BUFFER_ROWS = screen.getmaxyx()[0] - HEADER_ROWS
 
-    buffer_rows = screen.getmaxyx()[0] - HEADER_ROWS
     curses.noecho()  # Disable echoing of input
     curses.cbreak()  # React to keys instantly
     screen.nodelay(True)  # Set non-blocking mode
 
-    start = datetime.now()
+def main(screen):
+    """Main stopwatch function"""
+
+    init(screen)
+    start = dt.datetime.now()
     marks = [start]
-
     lap_num = 0
-
-    is_bold = False
 
     # Header
     for irow, line in enumerate(HEADER):
@@ -52,24 +62,25 @@ def main(screen):
                 sys.exit(0)
             if key == " ":  # mark a lap and move to the next row
                 lap_num += 1
-                marks.append(datetime.now())
+                marks.append(dt.datetime.now())
             if key == "u":  # undo last mark
-                #TODO can't undo past the start
+                if len(marks) < 2:
+                    continue
+                write_row(BLANK_LINE, lap_num, is_bold, screen)
                 lap_num -= 1
                 marks.pop(-1)
         except curses.error:
             # No key was pressed, print the time
-            now = datetime.now()
+            now = dt.datetime.now()
             since_mark = now - marks[-1]
             since_start = now - start
 
             # Toggle bold text when the display wraps to the top:
-            if lap_num % buffer_rows == 0:
-                is_bold = lap_num // buffer_rows % 2 != 0
+            if lap_num % BUFFER_ROWS == 0:
+                is_bold = lap_num // BUFFER_ROWS % 2 != 0
 
             line = f"{hms(now)}   {tds(since_mark)} (#{lap_num+1})   {tds(since_start)}"
-            row = HEADER_ROWS + lap_num % buffer_rows
-            screen.addstr(row, COL, line, fmt(is_bold))
+            write_row(line, lap_num, is_bold, screen)
 
 
 if __name__ == "__main__":
